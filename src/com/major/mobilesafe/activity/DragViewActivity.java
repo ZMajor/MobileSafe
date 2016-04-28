69db480751cbb9a831529acbbc89bfdbae7831ff
major <385994264@qq.com>
2016-04-28 17:19:37 +0800

    add addressService

--- src/com/major/mobilesafe/activity/DragViewActivity.java
+++ src/com/major/mobilesafe/activity/DragViewActivity.java
@@ -5,8 +5,10 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -30,6 +32,8 @@ public class DragViewActivity extends Activity {
 	private int startY;
 	private SharedPreferences mPref;
 	
+	long[] mHits = new long[2];// 数组长度表示要点击的次数
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -48,13 +52,47 @@ public class DragViewActivity extends Activity {
 		// ivDrag.layout(lastX, lastY, lastX + ivDrag.getWidth(),
 		// lastY + ivDrag.getHeight());
 		//不能用这个方法,因为还没有测量完成,就不能安放位置
-
+		
+		// 获取屏幕宽高
+		final int winWidth = getWindowManager().getDefaultDisplay().getWidth();
+		final int winHeight = getWindowManager().getDefaultDisplay()
+				.getHeight();
+		
+		if (lastY > winHeight / 2) {// 上边显示,下边隐藏
+			tvTop.setVisibility(View.VISIBLE);
+			tvBottom.setVisibility(View.INVISIBLE);
+		} else {
+			tvTop.setVisibility(View.INVISIBLE);
+			tvBottom.setVisibility(View.VISIBLE);
+		}
+		
 		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) ivDrag
 				.getLayoutParams();// 获取布局对象
 		layoutParams.leftMargin = lastX;// 设置左边距
 		layoutParams.topMargin = lastY;// 设置top边距
 
 		ivDrag.setLayoutParams(layoutParams);// 重新设置位置
+		
+		ivDrag.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+				mHits[mHits.length - 1] = SystemClock.uptimeMillis();// 开机后开始计算的时间
+				if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
+					// 把图片居中
+					ivDrag.layout(winWidth / 2 - ivDrag.getWidth() / 2,
+							ivDrag.getTop(), winWidth / 2 + ivDrag.getWidth()
+									/ 2, ivDrag.getBottom());
+					
+					// 记录坐标点
+					Editor edit = mPref.edit();
+					edit.putInt("lastX", winWidth / 2 - ivDrag.getWidth() / 2);
+					edit.putInt("lastY", ivDrag.getTop());
+					edit.commit();
+				}
+			}
+		});
 		
 		ivDrag.setOnTouchListener(new OnTouchListener() {
 			
@@ -81,6 +119,20 @@ public class DragViewActivity extends Activity {
 					int t = ivDrag.getTop() + dy;
 					int b = ivDrag.getBottom() + dy;
 					
+					// 判断是否超出屏幕边界, 注意状态栏的高度
+					if (l < 0 || r > winWidth || t < 0 || b > winHeight - 20) {
+						break;
+					}
+
+					// 根据图片位置,决定提示框显示和隐藏
+					if (t > winHeight / 2) {// 上边显示,下边隐藏
+						tvTop.setVisibility(View.VISIBLE);
+						tvBottom.setVisibility(View.INVISIBLE);
+					} else {
+						tvTop.setVisibility(View.INVISIBLE);
+						tvBottom.setVisibility(View.VISIBLE);
+					}
+					
 					// 更新界面
 					ivDrag.layout(l, t, r, b);
 					
@@ -100,7 +152,7 @@ public class DragViewActivity extends Activity {
 					break;
 				}
 				
-				return true;
+				return false;//事件要向下传递,让onclick(双击事件)可以响应
 			}
 		});
 		
--- src/com/major/mobilesafe/service/AddressService.java
+++ src/com/major/mobilesafe/service/AddressService.java
@@ -7,11 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -31,7 +35,13 @@ public class AddressService extends Service {
 	private WindowManager mWM;
 	private View view;
 	private SharedPreferences mPref;
-
+	
+	private int startX;
+	private int startY;
+	private int winWidth;
+	private int winHeight;
+	private WindowManager.LayoutParams params;
+	
 	@Override
 	public IBinder onBind(Intent arg0) {
 		return null;
@@ -103,21 +113,35 @@ public class AddressService extends Service {
 	}
 	
 	/**
-	 * 自定义归属地浮窗
+	 * 自定义归属地浮窗  需要权限android.permission.SYSTEM_ALERT_WINDOW
 	 */
 	private void showToast(String text) {
 		mWM = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
-
-		WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+		
+		// 获取屏幕宽高
+		winWidth = mWM.getDefaultDisplay().getWidth();
+		winHeight = mWM.getDefaultDisplay().getHeight();
+		
+		params = new WindowManager.LayoutParams();
 		params.height = WindowManager.LayoutParams.WRAP_CONTENT;
 		params.width = WindowManager.LayoutParams.WRAP_CONTENT;
 		params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-				| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
 				| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
 		params.format = PixelFormat.TRANSLUCENT;
-		params.type = WindowManager.LayoutParams.TYPE_TOAST;
+		//电话窗口。它用于电话交互（特别是呼入）。它置于所有应用程序之上，状态栏之下。
+		params.type = WindowManager.LayoutParams.TYPE_PHONE;
 		params.setTitle("Toast");
+		
+		// 将重心位置设置为左上方,也就是(0,0)从左上方开始,而不是默认的重心位置
+		params.gravity = Gravity.LEFT + Gravity.TOP;
+		
+		int lastX = mPref.getInt("lastX", 0);
+		int lastY = mPref.getInt("lastY", 0);
 
+		// 设置浮窗的位置, 基于左上方的偏移量
+		params.x = lastX;
+		params.y = lastY;
+		
 		//view = new TextView(this);
 		view = View.inflate(this, R.layout.toast_address, null);
 		
@@ -132,5 +156,69 @@ public class AddressService extends Service {
 		tvText.setText(text);
 
 		mWM.addView(view, params);// 将view添加在屏幕上(Window)
+		
+		view.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					// 初始化起点坐标
+					startX = (int) event.getRawX();
+					startY = (int) event.getRawY();
+					break;
+				case MotionEvent.ACTION_MOVE:
+					int endX = (int) event.getRawX();
+					int endY = (int) event.getRawY();
+
+					// 计算移动偏移量
+					int dx = endX - startX;
+					int dy = endY - startY;
+
+					// 更新浮窗位置
+					params.x += dx;
+					params.y += dy;
+
+					// 防止坐标偏离屏幕
+					if (params.x < 0) {
+						params.x = 0;
+					}
+
+					if (params.y < 0) {
+						params.y = 0;
+					}
+
+					// 防止坐标偏离屏幕
+					if (params.x > winWidth - view.getWidth()) {
+						params.x = winWidth - view.getWidth();
+					}
+
+					if (params.y > winHeight - view.getHeight()) {
+						params.y = winHeight - view.getHeight();
+					}
+
+					// System.out.println("x:" + params.x + ";y:" + params.y);
+
+					mWM.updateViewLayout(view, params);
+
+					// 重新初始化起点坐标
+					startX = (int) event.getRawX();
+					startY = (int) event.getRawY();
+					break;
+				case MotionEvent.ACTION_UP:
+					// 记录坐标点
+					Editor edit = mPref.edit();
+					edit.putInt("lastX", params.x);
+					edit.putInt("lastY", params.y);
+					edit.commit();
+					break;
+
+				default:
+					break;
+				}
+				return true;
+			}
+		});
+		
 	}
 }
